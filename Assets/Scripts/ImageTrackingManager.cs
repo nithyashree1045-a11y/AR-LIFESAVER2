0abede893101337063c88737963682cd5def23cb
nithyashree1045-a11y <nithyashree1045@gmail.com>
2026-07-14 22:30:36 +0530

    initial 1

--- Assets/Scripts/ImageTrackingManager.cs
+++ Assets/Scripts/ImageTrackingManager.cs
@@ -1,6 +1,32 @@
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ImageTrackingManager : MonoBehaviour
 {
-	public GameObject burnCube;
+    private ARTrackedImageManager trackedImageManager;
+
+    public GameObject burnCube;
+
+    private void Awake()
+    {
+        trackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        trackedImageManager.trackablesChanged.AddListener(OnTrackablesChanged);
+    }
+
+    private void OnDisable()
+    {
+        trackedImageManager.trackablesChanged.RemoveListener(OnTrackablesChanged);
+    }
+
+    private void OnTrackablesChanged(ARTrackablesChangedEventArgs<ARTrackedImage> args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Instantiate(burnCube, trackedImage.transform.position, trackedImage.transform.rotation);
+        }
+    }
 }
